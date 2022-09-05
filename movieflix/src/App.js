917ff04eff6e38e4aee22435d80fc02a5755e4d7
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-09-05 04:00:04 +0100

    Browse section

---
+++ movieflix/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Browse from "./pages/browse.js"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Browse />
+    </div>
+  );
+}
+
+export default App;
