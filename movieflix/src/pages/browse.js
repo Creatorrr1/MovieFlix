5ff49522b0b87a14151ea5c3b9bddaccf7a203e1
UnknownCreator1 <90321114+UnknownCreator1@users.noreply.github.com>
2022-09-08 10:47:55 +0100

    InnerHtml err needs to be resolved

--- movieflix/src/pages/browse.js
+++ movieflix/src/pages/browse.js
@@ -1,75 +1,90 @@
 import React from "react"
-import { useState } from "react"
 // import "../../App.css"
 import "./css/browse.css"
 function Browse() {
-	const [movies, setMovies] = useState("")
 
-	const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
-	const IMGPATH = "https://image.tmdb.org/t/p/w1280"
-	const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+  const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+  const IMGPATH = "https://image.tmdb.org/t/p/w1280"
+  const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
-	getMovies(APIURL)
+  const main = document.getElementById("main")
+  const form = document.getElementById("form")
+  const search = document.getElementById("search")
+  // initially get fav movies
+  getMovies(APIURL)
 
-	async function getMovies(url) {
-		const res = await fetch(url)
-		const resData = await res.json()
+  async function getMovies(url) {
+    const res = await fetch(url)
+    const resData = await res.json()
 
-		console.log(resData)
+    console.log(resData)
 
-		setMovies(resData.results)
-		// showMovies(resData.results)
-	}
+    showMovies(resData.results)
+  }
 
-	const getClassByRate = (vote) => {
-		if (vote >= 8) {
-			return "green"
-		} else if (vote >= 5) {
-			return "orange"
-		} else {
-			return "red"
-		}
-	}
+  function showMovies(movies) {
+    // clear main
+    // main.innerHTML = ""
+    // main = {__html: ""}
+    document.getElementById("main").innerHTML = ""
 
-	const searchHandle = (e) => {
-		e.preventDefault()
-		const { value } = e.target
+    movies.forEach((movie) => {
+      const { poster_path, title, vote_average, overview } = movie
 
-		const searchTerm = value
+      const movieEl = document.createElement("div")
+      movieEl.classList.add("movie")
 
-		if (searchTerm) {
-			getMovies(SEARCHAPI + searchTerm)
+      movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div className="movie-info">
+                <h3>${title}</h3>
+                <span className="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `
 
-			e.value = ""
-		}
-	}
+      main.appendChild(movieEl)
+    })
+
+    function getClassByRate(vote) {
+        if (vote >= 8) {
+            return "green";
+        } else if (vote >= 5) {
+            return "orange";
+        } else {
+            return "red";
+        }
+    }
     
-	return (
-		<>
-			{/* <h1>hello</h1> */}
-			<header>
-				<form id="form">
-					<input type="text" id="search" placeholder="Search" class="search" onSubmit={searchHandle} />
-				</form>
-			</header>
-			<main id="main">
-				{
-					movies.forEach((movie, index) => {
-						const { poster_path, title, vote_average, overview } = movie;
-                        <div className="movie" key={index}>
-							<img src={IMGPATH + poster_path} alt={title} />
-							<div className="movie-info">
-								<h3>${title}</h3>
-								<span className={getClassByRate(vote_average)}>${vote_average}</span>
-							</div>
-							<div className="overview">
-								<h3>Overview:</h3>${overview}
-							</div>
-						</div>
-					})}
-			</main>
-		</>
-	)
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+    
+        const searchTerm = search.value;
+    
+        if (searchTerm) {
+            getMovies(SEARCHAPI + searchTerm);
+    
+            search.value = "";
+        }
+    });
+  }
+  return (
+    <>
+      {/* <h1>hello</h1> */}
+      <header>
+        <form id="form">
+          <input type="text" id="search" placeholder="Search" class="search" />
+        </form>
+      </header>
+      <main id="main"></main>
+    </>
+  )
 }
 
 export default Browse
